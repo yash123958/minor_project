94222ec1d50d2fe9b805d1577eb24c2cb0eabe4d
Abhinav Kumar <136874461+Abhinav2508@users.noreply.github.com>
2026-09-11 21:36:00 +0530

    Abhinav (#4)
    * fixed role based access and their navigations
    
    * fixed all issue
    
    * fixed login issue

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -134,7 +134,6 @@ MAILERS = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
